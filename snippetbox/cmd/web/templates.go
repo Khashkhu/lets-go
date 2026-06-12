df0fef0db5348c3013a6dcc3458475fd8cb660cb
Egor <hashhumain@gmail.com>
2026-06-12 18:31:50 +0800

    Completed chapter 8 (added fully functional create section)

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 	"time"
 
+	"egorgodd.net/snippetbox/pkg/forms"
 	"egorgodd.net/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
